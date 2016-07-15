30829fbb8103a96029194bcca48e7136601e0d2e
wangjieshuai <wangjieshuai1990@gmail.com>
2016-07-15 18:02:01 +0800

    update chapter02 test
    Change-Id: I4be84c2a103073053f61c72893f952996c674a66

--- qt_practise/chapter02_test/dialog.cpp
+++ qt_practise/chapter02_test/dialog.cpp
@@ -2,11 +2,19 @@
 #include "ui_dialog.h"
 #include "QDebug"
 
+#include <QRegExp>
+
 Dialog::Dialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Dialog)
 {
     ui->setupUi(this);
+
+    connect(ui->button_ok, SIGNAL(clicked()), this, SLOT(accept()));
+    connect(ui->button_cancle, SIGNAL(clicked()), this, SLOT(reject()));
+
+    QRegExp regExp("[A-Za-z][1-9][0-9]{0,2}");
+    ui->lineEdit->setValidator(new QRegExpValidator(regExp, this));
 }
 
 Dialog::~Dialog()
@@ -18,17 +26,5 @@ void
 Dialog::on_lineEdit_textChanged()
 {
     qDebug() << "Dialog::on_lineEdit_changed";
-
-}
-
-void
-Dialog::on_button_ok_clicked()
-{
-    qDebug() << "Dialog::on_button_ok_clicked";
-}
-
-void
-Dialog::on_button_cancle_clicked()
-{
-    qDebug() << "Dialog::on_button_cancle_clicked";
+    ui->button_ok->setEnabled(ui->lineEdit->hasAcceptableInput());
 }
--- qt_practise/chapter02_test/dialog.h
+++ qt_practise/chapter02_test/dialog.h
@@ -17,8 +17,6 @@ public:
 
 private slots:
     void on_lineEdit_textChanged();
-    void on_button_ok_clicked();
-    void on_button_cancle_clicked();
 
 private:
     Ui::Dialog *ui;
