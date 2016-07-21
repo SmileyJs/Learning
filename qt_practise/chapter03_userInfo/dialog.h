77f07fab9ab3e4a93bf55203107f68e6396f26cc
wangjieshuai <wangjieshuai1990@gmail.com>
2016-07-21 18:10:02 +0800

    update
    Change-Id: I89c3adeaf306d60d3c53dbd5ae8dfc60957d4a91

--- qt_practise/chapter03_userInfo/dialog.h
+++ qt_practise/chapter03_userInfo/dialog.h
@@ -3,6 +3,12 @@
 
 #include <QDialog>
 
+class QLabel;
+class QTextEdit;
+class QComboBox;
+class QGridLayout;
+class QVBoxLayout;
+
 class Dialog : public QDialog
 {
     Q_OBJECT
@@ -10,6 +16,30 @@ class Dialog : public QDialog
 public:
     Dialog(QWidget *parent = 0);
     ~Dialog();
+
+private:
+    void initUserInfo();
+
+    // variables
+    QLabel *userName;
+    QTextEdit *userNameEdit;
+    QLabel *name;
+    QTextEdit *nameEdit;
+    QLabel *sex;
+    QComboBox *sexBox;
+    QLabel *department;
+    QTextEdit *departmentEdit;
+    QLabel *age;
+    QTextEdit *ageEdit;
+    QLabel *remark;
+    QGridLayout *leftLayout;
+    QLabel *head;
+    QLabel *headIcon;
+    QPushButton *updateButton;
+    QLabel *description;
+    QTextEdit *descriptionEdit;
+    QVBoxLayout *rightLayout;
+    QGridLayout *mainLayout;
 };
 
 #endif // DIALOG_H
