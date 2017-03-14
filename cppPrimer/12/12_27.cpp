8f2970f5fff3c340c0d5130d8905db380ab71506
wangjieshuai <wangjieshuai1990@gmail.com>
2017-03-14 20:13:09 +0800

    update
    Change-Id: I9febdcb32d5c15e7095e4dbdbc2821ca6e714da5

--- cppPrimer/12/12_27.cpp
+++ cppPrimer/12/12_27.cpp
@@ -1,21 +1,51 @@
 #include <iostream>
+#include <vector>
 #include <string>
 #include <sstream>
 #include <fstream>
+#include <map>
+#include <set>
 
 using namespace std;
 
 class TextQuery {
 public:
-	TExtQuery(const string &s);
+	TextQuery(ifstream &file);
+
+	// searchString(const string &s);
 
 private:
-
+	vector<string> data;
+	map<set<string>, vector<int>> wordMap;
 };
+
+TextQuery::TextQuery(ifstream &file)
+{
+	string line;
+	int lineNum = 0;
+
+	while (getline(file, line)) {
+		data.push_back(line);
+		++lineNum;
+
+		// istringstream iss(line);
+		// string word;
+		// while (line >> word) {
+		// 	wordMap[word]
+		// }
+	}
+}
 
 int
 main(int argc, char const *argv[])
 {
-	/* code */
+	ifstream file("bookList.txt");
+
+	if (!file) {
+		cout << "file open error!" << endl;
+		return -1; 
+	}
+
+	TextQuery text(file);
 	return 0;
 }
