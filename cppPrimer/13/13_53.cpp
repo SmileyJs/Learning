7ccfde5e1789b5305d5d5f400645c9aa2be004f3
wangjieshuai <wangjieshuai1990@gmail.com>
2017-03-31 20:15:34 +0800

    update
    Change-Id: If94ff493447ad851204bd36a05037ffd0fa78c99

--- cppPrimer/13/13_53.cpp
+++ cppPrimer/13/13_53.cpp
@@ -1,14 +1,20 @@
 #include "HasPtr.h"
+#include <chrono>
 
+using namespace std::chrono;
 int
 main(int argc, char const *argv[])
 {
-	HasPtr a("a");
-	HasPtr b("b");
+	HasPtr a("a", 1);
+	HasPtr b("b", 2);
 
 	cout << "before swap " << a.show() << " " << b.show() << endl;
-
-	swap(a, b);
+	auto t0 = high_resolution_clock::now();
+	for (int i = 0; i < 1000000; ++i) {
+		swap(a, b);
+	}
+	auto t1 = high_resolution_clock::now();
+	cout << duration_cast<milliseconds>(t1-t0).count() << "ms" << endl;
 
 	cout << "after swap " << a.show() << " " << b.show() << endl;
 
--- cppPrimer/13/HasPtr.h
+++ cppPrimer/13/HasPtr.h
@@ -7,11 +7,13 @@ class HasPtr {
 public:
 	friend bool operator<(const HasPtr &, const HasPtr &);
 	
-	HasPtr(const string &s = string()) : ps(new string(s)), i(0) {}
+	HasPtr(const string &s = string(), int n = 0) : ps(new string(s)), i(n) {}
 	HasPtr(const HasPtr &);
-	// HasPtr(HasPtr &&);
+	HasPtr(HasPtr &&) noexcept;
 
 	HasPtr &operator=(HasPtr);
+	// HasPtr &operator=(const HasPtr &);
+	// HasPtr &operator=(HasPtr &&) noexcept;
 
 	~HasPtr() { delete ps; }
 
@@ -27,11 +29,11 @@ private:
 void
 HasPtr::swap(HasPtr &lhs, HasPtr &rhs)
 {
-	cout << "HasPtr::swap() begin--left: " << *lhs.ps << " right: " << *rhs.ps << endl;
+	// cout << "HasPtr::swap() begin--left: " << *lhs.ps << lhs.i << " right: " << *rhs.ps << rhs.i << endl;
 	// using std::swap;
 	std::swap(lhs.ps, rhs.ps);
 	std::swap(lhs.i, rhs.i);
-	cout << "HasPtr::swap() end--left: " << *lhs.ps << " right: " << *rhs.ps << endl;
+	// cout << "HasPtr::swap() end--left: " << *lhs.ps << lhs.i << " right: " << *rhs.ps << rhs.i << endl;
 }
 
 bool
@@ -44,32 +46,51 @@ HasPtr::HasPtr(const HasPtr &c)
 	: ps(new string(*c.ps))
 	, i(c.i)
 {
-	cout << "HasPtr::HasPtr(const HasPtr &c)" << endl;
+	// cout << "HasPtr::HasPtr(const HasPtr &c) " << *c.ps << c.i << endl;
 }
 
-// HasPtr::HasPtr(HasPtr &&c)
-// 	: ps(c.ps)
-// 	, i(c.i)
-// {
-// 	cout << "HasPtr::HasPtr(HasPtr &&c)" << endl;
-// 	c.ps = nullptr;
-// }
+HasPtr::HasPtr(HasPtr &&c) noexcept
+	: ps(c.ps)
+	, i(c.i)
+{
+	// cout << "HasPtr::HasPtr(HasPtr &&c) "  << *c.ps << c.i << endl;
+	c.ps = nullptr;
+}
 
 HasPtr &
 HasPtr::operator=(HasPtr c)
 {
-	cout << "HasPtr &operator=(HasPtr c)" << endl;
+	// cout << "HasPtr &operator=(HasPtr c) " << *c.ps << c.i << endl;
 	swap(*this, c);
 	return *this;
-}
+}	
 
 // HasPtr &
-// HasPtr::operator=(const HasPtr &cp) {
+// HasPtr::operator=(const HasPtr &cp) 
+// {
+// 	// cout << "HasPtr::operator=(const HasPtr &cp)" << *cp.ps << cp.i << endl;
 // 	string *newPs = new string(*cp.ps);
 // 	// need to delete the orgin string
 // 	delete ps;
 // 	ps = newPs;
 // 	i = cp.i;
+
+// 	return *this;
+// }
+
+// HasPtr &
+// HasPtr::operator=(HasPtr &&c) noexcept
+// {
+// 	// cout << "HasPtr::operator=(HasPtr &&c)" << *c.ps << c.i << endl;
+
+// 	if (this != &c) {
+// 		delete ps;
+
+// 		ps = c.ps;
+// 		i = c.i;
+
+// 		c.ps = nullptr;
+// 	}
 
 // 	return *this;
 // }
