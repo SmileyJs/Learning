beafe2159990bce06250421814a4b724ea7d27b4
wangjieshuai <wangjieshuai1990@gmail.com>
2017-04-24 18:25:57 +0800

    udpate 15
    Change-Id: I365e205914909c7027e671f4462d09dc143b9b54

--- cppPrimer/15/Quote.cpp
+++ cppPrimer/15/Quote.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+
 class Quote 
 {
 public:
@@ -22,8 +23,6 @@ public:
 		cout << "data member: Quote" << "\n\tbookNo: " << bookNo << "\n\tprice: " << price << endl; 
 	}
 
-	virtual ~Quote() = default;
-
 protected:
 	double price = 0.0;
 
@@ -32,24 +31,41 @@ private:
 };
 
 double
-print_total(ostream &os, const Quote &item, size_t n) 
+print_total(ostream& os, const Quote& item, size_t n)
 {
-	double price = item.net_price(n);
+	double ret = item.net_price(n);
+	os << "bookNo: " << item.isbn() << "\tsold: " << n << "\ttotal_due: " << ret << endl;
 
-	os << "ISBN: " << item.isbn() << " #sold: " << n << "due: " << price << endl;
-
-	return price;
+	return ret;
 }
 
-class Bulk_Quote : public Quote 
+class Disc_Quote : public Quote
+{
+public:
+	Disc_Quote() = default;
+	Disc_Quote(const string& bookNo, double price, size_t n, double disc)
+		: Quote(bookNo, price), quantity(n), discount(disc) {}
+
+	virtual ~Disc_Quote() = default;
+
+	virtual double net_price(size_t n) const = 0;
+
+protected:
+	size_t quantity  = 0;
+	double discount = 0.0;
+};
+
+class Bulk_Quote : public Disc_Quote
 {
 public:
 	Bulk_Quote() = default;
-	Bulk_Quote(const string& book, double sales_price, size_t n, double discount)
-		: Quote(book, sales_price), discount(discount), min_qty(n) { }
+	Bulk_Quote(const string& bookNo, double price, size_t n, double disc)
+		: Disc_Quote(bookNo, price, n, disc) {}
 
-	virtual double net_price(size_t n ) const override {
-		if (n < min_qty) {
+	virtual ~Bulk_Quote() = default;
+
+	virtual double net_price(size_t n) const override {
+		if (n < quantity) {
 			return n * price;
 		}
 		else {
@@ -95,12 +111,15 @@ int
 main(int argc, char const *argv[])
 {
 	Quote a("aaa", 100);
-	Bulk_Quote b("bbb", 100, 10, 0.2);
-	Limit_Quote c("ccc", 100, 100, 0.2);
+
+	Bulk_Quote b("bbb", 100, 50, 0.2);
+	Limit_Quote c("ccc", 100, 50, 0.2);
 
 	print_total(cout, a, 70);
 	print_total(cout, b, 70);
 	print_total(cout, c, 70);
+
+	// Disc_Quote d("ddd", 100, 50, 0.2);
 
 	return 0;
 }
